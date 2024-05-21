1eaf91ba071e62c87c796ba25adb944bb46d66da
Sergey Kornienko <81762988+ocheweedno@users.noreply.github.com>
2024-05-21 08:11:26 +0300

    v.13.3

--- constants/pages/region/kuhnya-ryazanskogo-kraya.ts
+++ constants/pages/region/kuhnya-ryazanskogo-kraya.ts
@@ -3,9 +3,8 @@ export const template = [
     element: "LEAD_BLOCK",
     title:
       "Вопросом, что бы такого попробовать из местной кухни, задается любой путешественник. В 2018 году ответом всерьез озадачились и рязанские рестораторы. Итогом стал гастрономический фестиваль «Кухня Рязанского края», который теперь проходит каждую весну на площадках ведущих ресторанов города.",
-    desc: "Вопросом, что бы такого попробовать из местной кухни, задается любой путешественник. В 2018 году ответом всерьез озадачились и рязанские рестораторы. Итогом стал гастрономический фестиваль «Кухня Рязанского края», который теперь проходит каждую весну на площадках ведущих ресторанов города.\n\nКаждый ресторан-участник представляет особый гастрономический сет из региональных продуктов по фиксированной стоимости. Холодная и горячая закуски, первое блюдо, горячее с гарниром и десерт обходятся гостям заведений примерно в полторы тысячи рублей.\n\nЕсли вы решили, что опоздали к раздаче всего самого вкусного, и начали терять интерес, то не расстраивайтесь — лучшие находки входят в постоянное меню заведений и радуют туристов и местных жителей круглый год.",
-    /*  srcImg:
-       "https://s3-alpha-sig.figma.com/img/ab09/6574/3c6b4eaf7aed54992a017f8b04175a3f?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=N1LRV9kp~5T1nuaEQgFPV9BttjCYViG-S2MJ-jNH-rArcJ5W7~qCOMo2iqlDZ~YWkFlZ7xPICpIZMN9fVso0wVckAu11FUREj6TeuAOYqF0hAfyR9elz5xQ5CLr3TYGJ-qUVBr6Kx9C-~ZBMnfZUpSqVAZx7cpNOYv1aYjZ82dpNSY7SSVcAtwyLRPUfzFeV7ttg4rWB7-uPXaOO-RsHKy6Tb81EpHWpSHBqtBhC5Kj9y4Yp3WPkn2kq20~~K-gqkfsVzAOPbekhNVjBNNXSZ~QohwwuCDpx6dgDTo2byeB2FlejupnK3WVzAz5OLIairjFVsPBCHosENmtNQmJ4-g__", */
+    desc: "Каждый год с 1 по 31 мая Рязань особенно хлебосольна. В течение месяца шеф-повара местных ресторанов, пабов и кафе презентуют новое прочтение локальных продуктов и угощают жителей и гостей города неожиданными, но непременно вкусными блюдами высокой кухни.\n\nКаждый ресторан-участник представляет особый гастрономический сет из региональных продуктов стоимостью от 1500 до 2500 рублей. А презентация блюд помогает с неожиданной стороны взглянуть на историю и культуру Рязанского края.\n\nakrk62.ru",
+    srcImg: "https://i.ibb.co/D9LbynM/1.jpg",
   },
   {
     element: "QUOTE_BLOCK",
@@ -15,27 +14,28 @@ export const template = [
   },
   {
     element: "LONG_READ_BLOCK",
-    title: "Ресторан Графин",
+    title: " «Бристоль»",
     subElements: [
       {
         element: "LIST_BLOCK",
         list: [
-          "Сом со шпинатом",
-          "Котлета из щуки",
-          "Перепела",
-          "Калинник",
-          "Калач",
+          "Осетрина холодного копчения с соусом из щучьей икры и домашней сметаны на тостах из бородинского хлеба с семечками",
+          "Сливочная уха из судака с фенхелем и картофелем из костра",
+          "Стейк из языка, томленный в соусе из квасного сусла, с запеченной медовой тыквой и соусом из сливочного хрена",
+          "Рязанская клубника в бальзамическом соусе на панакоте из эстрагона с лимонным курдом и ванильным кремом",
         ],
-      },
-      {
-        element: "ADDRESS_BLOCK",
-        text: "Рязань, Татарская, 36, +7 (4912) 50 44 45",
-        link: "https://ya.ru",
       },
       {
         element: "LONG_READ_ITEM_BLOCK",
         srcImg:
-          "https://s3-alpha-sig.figma.com/img/23cd/0110/572c041669d7080512127233a2df3c17?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Au2anrm2re~R2PTqcEMOOeoz3~~p8sJ4P9mBqBQnm5y~viYkVuXcNCUX-BAAeLdb9jtNr5-rtxl-GSqhgN7fybRAwjz3X8jmPFjyipxH1J~fLmBCZ~T8dyXN2SRtj4FOLs97wFP5U~19w-TAMZA3ZoLK~v4glbEcOEm0vy3ydeSpcd3EXf1El-9Dy0ftPadA6b66poSdq2POb~FXbHIOtrFp5EaLEIS6o3iub1YeaeG-Sk5x-bYVF4TKP413VSSuqrnIlYYXRtvG6vRYlhVL86-yBnFVLV6cXOqYQ7ObI5f2inCdpNnR6OB7OOJtLIoMR16sfrIBEiWKVayalPDLHA__",
+          "https://i.ibb.co/4jB3HFx/2.jpg",
+      },
+      {
+        element: "LINK_BLOCK",
+        title: "«Бристоль»",
+        desc: "Рязань, Лыбедский бульвар, 1",
+        link: "https://hotel-oldtown.ru/restaurants/restoran-bristol/",
+        srcImg: "",
       },
     ],
   },
