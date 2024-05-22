d81badd764c7aba8122f408623d5ae3e48f091d7
Sergey Kornienko <81762988+ocheweedno@users.noreply.github.com>
2024-05-22 17:32:23 +0300

    v 15.3

--- app/ty-s-mestnym/obratnaya-svyaz/page.tsx
+++ app/ty-s-mestnym/obratnaya-svyaz/page.tsx
@@ -3,7 +3,7 @@ import Body from "./body";
 import Loading from "./loading";
 import { TLongReadBody } from "@/types/types";
 import { Suspense } from "react";
-import { template } from "@/constants/pages/ty-s-mestnym/mobilnoe-prilozhenie";
+import { template } from "@/constants/pages/ty-s-mestnym/obratnaya-svyaz";
 
 export const metadata: Metadata = {
   title: "Обратная связь - Всё о туризме в Рязани и Рязанской области",
