67a94f3662c87bafeb833e15c8c0096191d6be81
Sergey Kornienko <81762988+ocheweedno@users.noreply.github.com>
2024-07-31 13:23:29 +0300

    add novosti nasledie

---
+++ constants/pages/nasledie-kasimov/nasledie-kasimov.ts
@@ -0,0 +1,137 @@
+export const data = [
+  {
+    title: "Дом Шакуловых",
+    desc: "Касимов, Победы, 18",
+    imgUrl: "https://i.ibb.co/jTKxNLj/4D8A5100.jpg",
+  },
+  {
+    title: "Вознесенский собор",
+    desc: "Касимов, пл. Соборная",
+    imgUrl: "https://i.ibb.co/zRndmnB/4D8A5099.jpg",
+  },
+  {
+    title: "Церковь Успения Пресвятой Богородицы",
+    desc: "Касимов, пл. Соборная, 3",
+    imgUrl: "https://i.ibb.co/zRndmnB/4D8A5099.jpg",
+  },
+  {
+    title: "Дом Наставина",
+    desc: "Касимов, пл. Соборная, 17",
+    imgUrl: "https://i.ibb.co/8jSdHXq/4D8A5088.jpg",
+  },
+  {
+    title: "Центр Ишимбаева",
+    desc: "Касимов, Большакова, 31",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Три корпуса торговых рядов",
+    desc: "Касимов, пл. Соборная, 3б",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Старая татарская мечеть",
+    desc: "Касимов, пл. Победы, 9",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Дом Золотова",
+    desc: "Касимов, пл. Соборная, 10а",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Обелиски Петровской заставы",
+    desc: "Касимов, Набережная",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Текие Шах-Али хана",
+    desc: "Касимов, пл. Победы, 9б",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Троицкая церковь",
+    desc: "Касимов, Свердлова, 1",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Гимназия Мансуровой",
+    desc: "Касимов, Московская, 1",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Дом Пастернака",
+    desc: "Касимов, Карла Либкнехта, 9",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Кинотеатр «Марс»",
+    desc: "Касимов, Советская, 11а",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Благовещенская церковь",
+    desc: "Касимов, пл. Соборная, 13а",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Дом Смирнова",
+    desc: "Касимов, Советская, 7",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Дом Дьяконова",
+    desc: "Касимов, Советская, 15",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Дом Кастрова",
+    desc: "Касимов, Набережная, 77",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Дом Кастрова",
+    desc: "Касимов, Большакова, 33",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Дом Кастрова",
+    desc: "Касимов, пл. Победы",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Дом Качкова",
+    desc: "Касимов, Набережная, 35",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Дом Алянчиковых",
+    desc: "Касимов, пл. Соборная, 7/8",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Дом Салазкиных",
+    desc: "Касимов, Советская, 17",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Дом Шишкиных",
+    desc: "Касимов, Набережная, 34",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Дом Вереиных",
+    desc: "Касимов, Набережная, 29",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Дом Якунчиковых",
+    desc: "Касимов, Советская, 18",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+  {
+    title: "Никольская церковь",
+    desc: "Касимов, пл. Никольская, 23",
+    imgUrl: "https://i.ibb.co/6gyLv3H/4D8A4929.jpg",
+  },
+]
