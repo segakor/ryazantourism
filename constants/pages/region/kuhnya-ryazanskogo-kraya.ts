70c1637cd923d29f4cce0c5d6e53291d2364bf89
Sergey Kornienko <81762988+ocheweedno@users.noreply.github.com>
2024-08-28 22:00:01 +0300

    Update kuhnya-ryazanskogo-kraya.ts

--- constants/pages/region/kuhnya-ryazanskogo-kraya.ts
+++ constants/pages/region/kuhnya-ryazanskogo-kraya.ts
@@ -29,7 +29,7 @@ export const template = [
       {
         element: "LONG_READ_ITEM_BLOCK",
         text: "<ul><li>Телячьи щеки томленые</li><li>Котлета из щуки</li><li>Перепела</li><li>Пшеничная катанка с белыми грибами</li><li>Калинник</li><li>Калач</li></ul>",
-        srcImg: "",
+        srcImg: "https://i.ibb.co/YTLJmx8/rhr1.jpg",
       },
       {
         element: "LINK_BLOCK",
@@ -47,13 +47,85 @@ export const template = [
       {
         element: "LONG_READ_ITEM_BLOCK",
         text: "<ul><li>Перепел запеченный в капустном листе</li><li>Томленая утиная ножка с морковью и пюре из моркови с облепихой</li><li>Перловая каша с белыми грибами и крошкой из бородинского хлеба</li><li>Калинник с мороженым соленая карамель</li></ul>",
-        srcImg: "",
+        srcImg: "https://i.ibb.co/CtNcGZM/rhr2.jpg",
       },
       {
         element: "LINK_BLOCK",
         title: "Гастробар «Есть»",
         desc: "Рязань, Почтовая, 62, +7 910 560 05 04",
         link: "https://vk.com/est_gastrobar",
+        srcImg: "",
+      },
+    ],
+  },
+  {
+    element: "LONG_READ_BLOCK",
+    title: "Ресторан «Буфетъ»",
+    subElements: [
+      {
+        element: "LONG_READ_ITEM_BLOCK",
+        text: "<ul><li>Грибные пончики с хвойной сметаной</li><li>Суп грибной со сметаной, гречневым хлебцем с грибным паштетом</li><li>Тушеные говяжьи щечки, печеный маринованный помидор</li></ul>",
+        srcImg: "https://i.ibb.co/mTZQc84/rhr3.jpg",
+      },
+      {
+        element: "LINK_BLOCK",
+        title: "Ресторан «Буфетъ»",
+        desc: "Рязань, Радищева, 42, БЦ «Зарядский», 1 этаж, +7 (4912) 77 94 44",
+        link: "https://vk.com/bufet_rzn",
+        srcImg: "",
+      },
+    ],
+  },
+  {
+    element: "LONG_READ_BLOCK",
+    title: "Трактир «Белый»",
+    subElements: [
+      {
+        element: "LONG_READ_ITEM_BLOCK",
+        text: "<ul><li>Паштет из печени птицы</li><li>Щи томленые</li><li>Похлебка грибная с полбой</li><li>Рагу с кроликом и муссом из печеного чеснока</li></ul>",
+        srcImg: "https://i.ibb.co/K6n1Hyt/rhr4.jpg",
+      },
+      {
+        element: "LINK_BLOCK",
+        title: "Трактир «Белый»",
+        desc: "Рязань, Николодворянская, 18, +7 (4912) 77 70 16",
+        link: "https://xn--90aqf3dg.xn--p1acf/",
+        srcImg: "",
+      },
+    ],
+  },
+  {
+    element: "LONG_READ_BLOCK",
+    title: "Ресторан «Боровница»",
+    subElements: [
+      {
+        element: "LONG_READ_ITEM_BLOCK",
+        text: "<ul><li>Щи по-рязански томленые в русской печи</li><li>Каша из полбы по-мещёрски с говядиной и овощами/с судаком</li><li>Скоблянка</li></ul>",
+        srcImg: "",
+      },
+      {
+        element: "LINK_BLOCK",
+        title: "Ресторан «Боровница»",
+        desc: "Рязань, пос. Солотча, пл. Монастырская, 8, +7 (4912) 28 79 63",
+        link: "http://borovnica.ru/restoran/",
+        srcImg: "",
+      },
+    ],
+  },
+  {
+    element: "LONG_READ_BLOCK",
+    title: "Сеть кафе «Старый Мельник»",
+    subElements: [
+      {
+        element: "LONG_READ_ITEM_BLOCK",
+        text: "<ul><li>Курник</li><li>Перепела с брусничным соусом</li><li>Сом из печи</li><li>Калинник</li></ul>",
+        srcImg: "https://i.ibb.co/hXCDRJn/rhr5.jpg",
+      },
+      {
+        element: "LINK_BLOCK",
+        title: "Сеть кафе «Старый Мельник»",
+        desc: "«Старый Мельник. Гнездо», Рязань, пос. Солотча, Солотчинское шоссе, 68, «Старый Мельник. Старица», Рязань, пос. Солотча, Владимировская, 95а, стр. 1, «Старый Мельник. ФаЗООнда», Рязанский р-н, с. Поляны, Новая, 36, корп. 4, +7 (4912) 77 71 72",
+        link: "https://stmelnik.ru/",
         srcImg: "",
       },
     ],
