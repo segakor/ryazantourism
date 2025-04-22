3099bd1cd3c3b59bec557a0c14c0c161423938ad
Sergey Kornienko <81762988+ocheweedno@users.noreply.github.com>
2025-04-22 15:01:32 +0300

    Update ModalRyazanPlus.tsx

--- components/modules/ModalOrder/ModalRyazanPlus.tsx
+++ components/modules/ModalOrder/ModalRyazanPlus.tsx
@@ -71,8 +71,9 @@ export const ModalRyazanPlus = () => {
               прогулка с экскурсоводом, полезные подарки и скидки, которые
               обязательно пригодятся во время путешествия. <br />
               <br />
-              Каждую субботу с конца апреля по конец октября мы встречаем гостей
-              города около «АМАКС Конгресс-отель» (Первомайский просп., 54).
+              Каждую субботу мы встречаем гостей города на улице Почтовой.
+              <br />
+              <br />
               Время старта 12.00.
             </div>
           </div>
@@ -169,16 +170,14 @@ export const ModalRyazanPlus = () => {
                       {[1, 2, 3, 4, 5].map((item, index) => (
                         <button
                           type="button"
-                          className={`cursor-pointer rounded-xl transition-all p-3 bg-[#998ce4] ${
-                            count === item && "!bg-white"
-                          }`}
+                          className={`cursor-pointer rounded-xl transition-all p-3 bg-[#998ce4] ${count === item && "!bg-white"
+                            }`}
                           key={index}
                           onClick={() => onChangeCount(item)}
                         >
                           <div
-                            className={`min-w-[1.875rem] font-medium text-center text-white ${
-                              count === item && "!text-black"
-                            }`}
+                            className={`min-w-[1.875rem] font-medium text-center text-white ${count === item && "!text-black"
+                              }`}
                           >
                             {item != 5 ? item : item + "+"}
                           </div>
@@ -199,17 +198,15 @@ export const ModalRyazanPlus = () => {
                       <input
                         {...input}
                         {...meta}
-                        className={`relative float-left -ms-[1.5rem] me-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid  outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-checkbox before:shadow-transparent before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ms-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-black/60 focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-black/60 focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-checkbox checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ms-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent rtl:float-right dark:border-neutral-400 dark:checked:border-primary dark:checked:bg-primary ${
-                          !!meta.touched && !!meta.error && "border-[#f31260]"
-                        }`}
+                        className={`relative float-left -ms-[1.5rem] me-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid  outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-checkbox before:shadow-transparent before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ms-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-black/60 focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-black/60 focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-checkbox checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ms-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent rtl:float-right dark:border-neutral-400 dark:checked:border-primary dark:checked:bg-primary ${!!meta.touched && !!meta.error && "border-[#f31260]"
+                          }`}
                         type="checkbox"
                         value=""
                         id="checkboxDefault"
                       />
                       <label
-                        className={`inline-block ps-[0.15rem] text-sm hover:cursor-pointer ${
-                          !!meta.touched && !!meta.error && "!text-[#f31260]"
-                        }`}
+                        className={`inline-block ps-[0.15rem] text-sm hover:cursor-pointer ${!!meta.touched && !!meta.error && "!text-[#f31260]"
+                          }`}
                       >
                         Даю согласие на обработку персональных данных
                       </label>
